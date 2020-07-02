42193bfe9cd35c580a677143ece8d5fa1724a7a6
rodrigobdias <rodrigobdias@gmail.com>
2020-07-02 11:39:17 -0300

    Distributed Tracing

--- src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.fornecedor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,15 @@ import br.com.alura.microservice.fornecedor.service.InfoService;
 @RequestMapping("/info")
 public class InfoController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(InfoController.class);
+	
 	@Autowired
 	private InfoService infoService;
 	
 	@GetMapping("/{estado}")
 	public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
+		
+		LOG.info("recebido pedido de informações do fornecedor de {}", estado);
 		
 		return infoService.getInfoPorEstado(estado);
 		
--- src/main/java/br/com/alura/microservice/fornecedor/controller/PedidoController.java
+++ src/main/java/br/com/alura/microservice/fornecedor/controller/PedidoController.java
@@ -2,6 +2,8 @@ package br.com.alura.microservice.fornecedor.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +20,16 @@ import br.com.alura.microservice.fornecedor.service.PedidoService;
 @RequestMapping("/pedido")
 public class PedidoController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
+	
 	@Autowired
 	private PedidoService pedidoService;
 	
 	@PostMapping
 	public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
+		
+		LOG.info("pedido recebido ");
+		
 		return pedidoService.realizaPedido(produtos);
 	}
 	
