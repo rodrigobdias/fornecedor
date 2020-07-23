8ca729c5044adc767a707a3e91595e15230e3343
rodrigobdias <rodrigobdias@gmail.com>
2020-07-22 22:24:19 -0300

    Documentação do microservice com SpringFox Swagger

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # fornecedor
+
+Projeto desenvolvido no curso de Microservices com Spring Cloud
+
+Projetos que fazem parte desse curso:
+
+- loja
+
+- transportador
+
+- zuul
+
+- auth
+
+- config-server
+
+- microservice-settings
+
+- eureka-server
+
+Documentação da API via Swagger
+
+![Screenshot](screenshot-1.png)
--- src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
+++ src/main/java/br/com/alura/microservice/fornecedor/FornecedorApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableResourceServer
+@EnableSwagger2
 public class FornecedorApplication {
 
 	public static void main(String[] args) {
